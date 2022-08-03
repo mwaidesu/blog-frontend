95666a4f5a3e4a9b9023cf0c3e68e439db447bb1
Mwai <machariamwai@gmail.com>
2022-08-03 17:55:18 +0300

    First commit

---
+++ src/AppState.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { createContext } from "react";
+// import App from "./App";
+
+const initialState = {
+  url: "http://blog-back-end-rails.herokuapp.com"
+};
+
+///////
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//
+const AppContext = React.createContext(null)
+//
+export const AppState = (props) =>{
+const [state, dispatch] = useReducer(reducer, initialState);
+  return (<AppContext.Provider value={{state, dispatch}}>
+    {props.children}
+  </AppContext.Provider>);
+}
+
+/////
+export const useAppState = () =>{
+  return React.useContext(AppContext)
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Router } from 'react-router-dom';
+import { AppState } from './AppState';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppState>
+      <Router>
+        <App />
+      </Router>
+    </AppState>
+  </React.StrictMode>
+);
+
